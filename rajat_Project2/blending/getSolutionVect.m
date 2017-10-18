c560b8f0c2b01eee31e3c26fc8ef932804cb0695
Rajat Bhageria <rajat1996@gmail.com>
2017-10-18 15:10:08 -0400

    create soln vector

--- rajat_Project2/blending/getSolutionVect.m
+++ rajat_Project2/blending/getSolutionVect.m
@@ -1,3 +1,41 @@
 function solVectorb = getSolutionVect(indexes, source, target, offsetX, offsetY)
 %% Enter Your Code Here
+%% Calculate the masked gradient angles and magnitude of source 
+IxSource = conv2(source, [1,0,-1]); 
+IySource = conv2(source, [1,0,-1]'); 
+
+mask = maskImage(source); 
+
+gradientMagSource = (IxSource^2 + IySource^2)^.5;
+gradientAnglesSource = atan2(IySource, IxSource); 
+
+maskedGradientMagSource = gradientMagSource * mask; 
+maskedGradientAnglesSource = gradientAnglesSource * mask; 
+
+%% Calculate the masked gradient angles and magnitude of target 
+IxTarget = conv2(target, [1,0,-1]); 
+IyTarget = conv2(target, [1,0,-1]'); 
+
+inverseMask = ~mask;
+
+gradientMagTarget = (IxTarget^2 + IyTarget^2)^.5;
+gradientAnglesTarget = atan2(IyTarget, IxTarget); 
+
+maskedGradientMagTarget = gradientMagTarget * inverseMask; 
+maskedGradientAnglesTarget = gradientAnglesTarget * inverseMask; 
+
+%% Copy the gradient mag and source of masked source to target 
+finalGradientMag = maskedGradientMagSource + maskedGradientMagTarget; 
+finalGradientAngles = maskedGradientAnglesSource + maskedGradientAnglesTarget; 
+
+%% Compute the final gradient in x and y directions 
+IxFinal = finalGradientMag *cos(finalGradientAngles); 
+IyFinal = finalGradientMag *sin(finalGradientAngles); 
+
+%% Find the vinal vector b 
+bTop = conv2(IxFinal, [1,0,-1]'); 
+bBottom = conv2(IyFinal, [1,0,-1]); 
+
+%% Return the final b 
+solVectorb = [bTop;bBottom];
 end
--- rajat_Project2/blending/reconstructImg.m
+++ rajat_Project2/blending/reconstructImg.m
@@ -1,3 +1,19 @@
 function resultImg = reconstructImg(indexes, red, green, blue, targetImg)
 %% Enter Your Code Here
+redSolnb = getSolutionVect(indexes, red, targetImg, offsetX, offsetY); 
+greenSolnb = getSolutionVect(indexes, green, targetImg, offsetX, offsetY); 
+blueSolnb = getSolutionVect(indexes, blue, targetImg, offsetX, offsetY); 
+
+coeffA = getCoefficientMatrix(indexes);
+
+xRed = coeffA \ redSolnb; 
+xGreen = coeffA \ greenSolnb; 
+xBlue = coeffA \ blueSolnb; 
+
+redReshape = reshape(xRed,size(targetImg));
+greenReshape = reshape(xGreen,size(targetImg));
+blueReshape = reshape(xBlue,size(targetImg));
+
+resultImg = cat(3, redReshape, greenReshape, blueReshape);  
+
 end
